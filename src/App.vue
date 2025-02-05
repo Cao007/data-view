ffedbba4791f8625b19966f9c511e95552bf73df
Cao007 <1114685490@qq.com>
2025-02-06 01:09:34 +0800

    feat: 顶部数据总览

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div class="app-container">
     <Header></Header>
     <div class="main">
-      123
+      <TopShowData :topData="topData"></TopShowData>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 import Header from './components/header/index.vue'
+import TopShowData from './components/topShowData/index.vue'
 import { gather, charge, outPatient, hospitalization, hospitalPostion } from '@/api/index'
+
+// 一次性获取所有数据
 const getAllData = async () => {
   const gatherData = await gather()
   const chargeData = await charge()
@@ -19,16 +22,34 @@ const getAllData = async () => {
   const hospitalPostionData = await hospitalPostion()
 
   return {
-    gatherData,
-    chargeData,
-    outPatientData,
-    hospitalizationData,
-    hospitalPostionData
+    gatherData: gatherData.data.data,
+    chargeData: chargeData.data.data,
+    outPatientData: outPatientData.data.data,
+    hospitalizationData: hospitalizationData.data.data,
+    hospitalPostionData: hospitalPostionData.data.data
   }
 }
 
+// 获取顶部数据
+const topData = reactive([])
+
 getAllData().then(res => {
   console.log(res)
+  topData.push(
+    {
+      total: res.gatherData.allgather,
+      month: res.gatherData.monthegather
+    },
+    {
+      total: res.hospitalizationData.allHospitalization,
+      month: res.hospitalizationData.montHospitalization
+    },
+    {
+      total: res.outPatientData.allOutpatient,
+      month: res.outPatientData.monthOutpatient
+    }
+  )
+
 })
 </script>
 
--- src/components/topShowData/index.vue
+++ src/components/topShowData/index.vue
@@ -1,17 +1,107 @@
 <template>
-  <div class="show-data-container">
-    {{ name }}
+  <div class="top-show-data-container">
+    <div class="box-item" v-for="(item, index) in topDataConf" :key="index">
+      <!-- 总采集 -->
+      <div class="total-box">
+        <div class="left-icon">
+          <img :src="item.totalIcon" alt="" />
+        </div>
+        <div class="right-desc">
+          <h3>{{ item.totalTitle }}</h3>
+          <h2 :style="{ color: item.color }">{{ props.topData[index]?.total }}</h2>
+        </div>
+      </div>
+
+      <!-- 当月采集 -->
+      <div class="month-box">
+        <div class="left-icon">
+          <img :src="item.monthIcon" alt="" />
+        </div>
+        <div class="right-desc">
+          <h3>{{ item.monthTitle }}</h3>
+          <h2 :style="{ color: item.color }">{{ props.topData[index]?.month }}</h2>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-const name = ref('顶部数据展示')
+const topDataConf = [
+  {
+    totalTitle: "总采集数据量",
+    monthTitle: "当月采集量",
+    totalIcon: "./info_1.png",
+    monthIcon: "./info_2.png",
+    color: "#ffff43"
+  },
+  {
+    totalTitle: "总住院数据量",
+    monthTitle: "当月住院集量",
+    totalIcon: "./info_3.png",
+    monthIcon: "./info_4.png",
+    color: "#25ebca"
+  },
+  {
+    totalTitle: "总门诊数据量",
+    monthTitle: "当月门诊集量",
+    totalIcon: "./info_5.png",
+    monthIcon: "./info_6.png",
+    color: "#ff4e4e"
+  },
+]
+
+const props = defineProps(['topData'])
 
 </script>
 
 <style lang="scss" scoped>
-.show-data-container {
-  color: pink;
+.top-show-data-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+
+  .box-item {
+    display: flex;
+    align-items: center;
+    padding: 1.3vw;
+    box-sizing: border-box;
+    width: 31.66vw;
+    height: 11.48vh;
+    background-color: #034c6a;
+
+    .total-box,
+    .month-box {
+      display: flex;
+      align-items: center;
+      flex: 1;
+
+      .left-icon {
+        width: 2vw;
+        height: 2vw;
+        margin-right: 1vw;
+
+        img {
+          width: 100%;
+          height: 100%;
+        }
+      }
+
+      .right-desc {
+        h3 {
+          margin: 0;
+          font-size: 0.8vw;
+          color: #fff;
+        }
+
+        h2 {
+          margin: 0;
+          font-size: 1.2vw;
+          color: #fff;
+        }
+      }
+    }
+  }
 }
 </style>
